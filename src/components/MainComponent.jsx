00c7fa3051b197bca5dc4bedfc85038a3f3c5a15
TahaGPT <tahaaahmed12115@gmail.com>
2025-06-07 17:19:43 +0500

    Adding an Ingredient with States

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,17 +1,34 @@
+import React from "react";
+
 export default function MainComponent() {
+  const [ingredients, setIngredients] = React.useState([]);
+
+  const ingredientList = ingredients.map(function (ingredient) {
+    return <li key={ingredient}>{ingredient}</li>;
+  });
+
+  function addItem(event) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    setIngredients(function (prevIngredientList) {
+      return [...prevIngredientList, newIngredient];
+    });
+  }
+
   return (
     <main>
-      <form className = "FormDiv">
+      <form className="FormDiv" onSubmit={addItem}>
         <input
           className="FormInput"
           type="text"
           placeholder="e.g. Chilli"
           aria-label="Add an ingredient"
+          name="ingredient"
         />
-        <button className="Button">
-          + Add Ingredient
-        </button>
+        <button className="Button">+ Add Ingredient</button>
       </form>
+      <ul>{ingredientList}</ul>
     </main>
   );
 }
